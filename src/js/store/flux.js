fc844837ffe62e08086be433bf8cee08ff932522
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-08-20 00:55:50 +0000

    flux y context

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,161 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			People: null,
+			Vehicles: null,
+			Planets: null,
+			Species: null,
+			Films: null,
+			Starships: null,
+			Character: null
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			loadPeople: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ People: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			loadVehicles: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ Vehicles: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			loadPlanets: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ Planets: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			loadSpecies: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ Species: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			loadFilms: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ Films: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			loadStarships: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ Starships: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			loadCharacter: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ Character: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			loadFavorites: async (url) => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    if (response.status !== 200) throw new Error("Error API");
+                    const data = await response.json();
+                    setStore({ Favorites: data });
+                } catch (error) {
+                    setStore({
+                        error: error.message
+                    });
+                }
+            },
+			addFavorites: (element) => {
+				let { Favorites } = getStore();
+				Favorites.push(element);
+				setStore({Favorites});
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			removeFavorites: (element) => {
+				let { Favorites } = getStore();
+                let  list = Favorites.filter(item => item !== element);
+                setStore({Favorites: list});
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
